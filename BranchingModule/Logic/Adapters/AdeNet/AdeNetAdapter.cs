2ca71fd481a8bf2252b0be3ffbfa09a2d78465f5
nim <niklaus.maurer@m-s.ch>
2015-08-07 19:35:00 +0200

    ADDED: AdeNetAdapter to SourceControl

---
+++ BranchingModule/Logic/Adapters/AdeNet/AdeNetAdapter.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace BranchingModule.Logic
+{
+	internal class AdeNetAdapter : IAdeNetAdapter
+	{
+		#region Properties
+		private IConvention Convention { get; set; }
+		private ISettings Settings { get; set; }
+		#endregion
+
+		#region Constructors
+		public AdeNetAdapter(IConvention convention, ISettings settings)
+		{
+			if(convention == null) throw new ArgumentNullException("convention");
+			if(settings == null) throw new ArgumentNullException("settings");
+
+			this.Convention = convention;
+			this.Settings = settings;
+		}
+		#endregion
+
+		#region Publics
+		public void InstallPackages(BranchInfo branch)
+		{
+			string strArguments = string.Format(@"/C {0}\AdeNet.exe -workingdirectory {1} -deploy -development", this.Settings.AdeNetExePath, this.Convention.GetLocalPath(branch));
+
+			ProcessStartInfo startInfo = new ProcessStartInfo("cmd.exe")
+			                             {
+				                             Arguments = strArguments,
+				                             CreateNoWindow = true,
+											 UseShellExecute = false
+			                             };
+
+			using(Process process = Process.Start(startInfo))
+			{
+				if(process != null) process.WaitForExit();
+			}
+		}
+		#endregion
+	}
+}
