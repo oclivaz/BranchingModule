9ec146c6638eef97690d316292f5fe7c43d479ec
nim <niklaus.maurer@m-s.ch>
2015-12-07 10:59:45 +0100

    CHANGED: Text Output included in retry-block

--- BranchingModule/Logic/Services/Dump/DumpService.cs
+++ BranchingModule/Logic/Services/Dump/DumpService.cs
@@ -85,10 +85,10 @@ namespace BranchingModule.Logic
 			if(strDump == null) throw new ArgumentNullException("strDump");
 			if(strDB == null) throw new ArgumentNullException("strDB");
 
-			this.TextOutput.WriteVerbose(string.Format("Restoring {0} into {1}", strDump, strDB));
-
 			Retry.Do(() =>
 			         {
+						 this.TextOutput.WriteVerbose(string.Format("Restoring {0} into {1}", strDump, strDB));
+
 				         this.SQLServer.ExecuteScript(GetKillConnectionsScript(strDB), MASTER);
 				         this.SQLServer.ExecuteScript(GetRestoreDatabaseScript(strDump, strDB), MASTER);
 				         this.SQLServer.ExecuteScript(GetPostRestoreScript(strDB), strDB);
